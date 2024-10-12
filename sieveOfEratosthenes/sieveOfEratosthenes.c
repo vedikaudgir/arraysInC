0dd8e03e201c1cb5e9714941ca7f046b6f54fcfa
Vedika Udgir <vedikaudgir@gmail.com>
2024-10-12 22:03:32 +0530

    Add files via upload

---
+++ sieveOfEratosthenes/sieveOfEratosthenes.c
@@ -0,0 +1,36 @@
+// Program to print all prime numbers till 100 using the method of sieve Of Eratosthenes.
+// Author - Vedika Udgir.
+
+#include <stdio.h>
+
+void printMatrice(int a[10][10])
+{
+    int i, j;
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 1; j <= 10; j++)
+        {
+            printf("%4d", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void main()
+{
+    int i, j, m[10][10];
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 1; j <= 10; j++)
+        {
+            m[i][j] = (i * 10) + j;
+            if ((m[i][j] % 2 == 0 && m[i][j] != 2) || (m[i][j] % 3 == 0 && m[i][j] != 3) || (m[i][j] % 5 == 0 && m[i][j] != 5) || (m[i][j] % 7 == 0 && m[i][j] != 7))
+            {
+                m[i][j] = 0;
+            }
+        }
+    }
+    printf("prime numbers till 100 are - \n");
+    printMatrice(m);
+}
